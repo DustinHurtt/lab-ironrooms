e004a16df376d9b1e1f63c4f954167b22aab58fe
Dustin Hurtt <dustin_hurtt@outlook.com>
2022-11-18 11:42:54 -0500

    first commit

---
+++ config/session.config.js
@@ -0,0 +1,33 @@
+// require session
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+
+require("dotenv/config");
+
+
+module.exports = app => {
+
+  app.set('trust proxy', 1);
+
+  // use session
+  app.use(
+    session({
+      secret: process.env.SESS_SECRET,
+      resave: true,
+      saveUninitialized: false,
+      cookie: {
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+        secure: process.env.NODE_ENV === 'production',
+        httpOnly: true,
+        maxAge: 6000000 // 60 * 1000 ms === 1 min
+      },
+      store: MongoStore.create({
+        mongoUrl:
+          process.env.MONGODB_URI ||
+          "mongodb://localhost/lab-ironrooms",
+
+
+      }),
+    })
+  );
+};
