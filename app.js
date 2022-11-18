05a02ab7f451c7f7d81ff6f10807cc284c93e003
Dustin Hurtt <dustin_hurtt@outlook.com>
2022-11-18 14:32:14 -0500

    beginning auth routes

--- app.js
+++ app.js
@@ -25,9 +25,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var authRouter = require('./routes/auth');
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/auth', authRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,6 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource WOWZERS!!!!');
 });
 
+
+
 module.exports = router;
